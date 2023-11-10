b04e6317ce5f4fda6b8732834766265c2b58ac4c
Lukas Kruegle <lukaskruegle@gmail.com>
2023-11-10 14:19:39 -0500

    Add vim-abolish

--- init.lua
+++ init.lua
@@ -76,6 +76,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  'tpope/vim-abolish',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
