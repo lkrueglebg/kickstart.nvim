b54d8f762fa91f57b4e5d4b927256daacf9b0545
Lukas Kruegle <lukas.kruegle@berkshiregrey.com>
2024-02-26 12:55:49 -0500

    Disable obsidian and pomo plugins

--- init.lua
+++ init.lua
@@ -272,89 +272,89 @@ require('lazy').setup({
   },
 
   -- Obsidian support
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    ft = "markdown",
-    -- Potentially use this to load only when in a vault
-    -- event = {
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- }
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",
+  --   ft = "markdown",
+  --   -- Potentially use this to load only when in a vault
+  --   -- event = {
+  --   --   "BufReadPre path/to/my-vault/**.md",
+  --   --   "BufNewFile path/to/my-vault/**.md",
+  --   -- }
+  --
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     'hrsh7th/nvim-cmp',
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = "iBreadless",
+  --         path = "Library/Mobile Documents/iCloud~md~obsidian/Documents/iBreadless"
+  --       },
+  --     }
+  --   },
+  --
+  -- },
 
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      'hrsh7th/nvim-cmp',
-      "nvim-telescope/telescope.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-
-    opts = {
-      workspaces = {
-        {
-          name = "iBreadless",
-          path = "Library/Mobile Documents/iCloud~md~obsidian/Documents/iBreadless"
-        },
-      }
-    },
-
-  },
-
-  {
-    "epwalsh/pomo.nvim",
-    version = "*",
-    lazy = true,
-    cmd = { "TimerStart", "TimerRepeat" },
-    dependencies = {
-      "rcarriga/nvim-notify",
-    },
-    opts = {
-      -- How often the notifiers are updated.
-      update_interval = 1000,
-
-      -- Configure the default notifiers to use for each timer.
-      -- You can also configure different notifiers for timers given specific names, see
-      -- the 'timers' field below.
-      notifiers = {
-        -- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
-        {
-          name = "Default",
-          opts = {
-            -- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
-            -- continuously displayed. If you only want a pop-up notification when the timer starts
-            -- and finishes, set this to false.
-            sticky = true,
-
-            -- Configure the display icons:
-            title_icon = "󱎫",
-            text_icon = "󰄉",
-            -- Replace the above with these if you don't have a patched font:
-            -- title_icon = "⏳",
-            -- text_icon = "⏱️",
-          },
-        },
-
-        -- The "System" notifier sends a system notification when the timer is finished.
-        -- Currently this is only available on MacOS.
-        -- Tracking: https://github.com/epwalsh/pomo.nvim/issues/3
-        -- { name = "System" },
-
-        -- You can also define custom notifiers by providing an "init" function instead of a name.
-        -- See "Defining custom notifiers" below for an example 👇
-        -- { init = function(timer) ... end }
-      },
-
-      -- Override the notifiers for specific timer names.
-      timers = {
-        -- For example, use only the "System" notifier when you create a timer called "Break",
-        -- e.g. ':TimerStart 2m Break'.
-        Break = {
-          { name = "System" },
-        },
-      },
-    }
-
-  },
+  -- {
+  --   "epwalsh/pomo.nvim",
+  --   version = "*",
+  --   lazy = true,
+  --   cmd = { "TimerStart", "TimerRepeat" },
+  --   dependencies = {
+  --     "rcarriga/nvim-notify",
+  --   },
+  --   opts = {
+  --     -- How often the notifiers are updated.
+  --     update_interval = 1000,
+  --
+  --     -- Configure the default notifiers to use for each timer.
+  --     -- You can also configure different notifiers for timers given specific names, see
+  --     -- the 'timers' field below.
+  --     notifiers = {
+  --       -- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
+  --       {
+  --         name = "Default",
+  --         opts = {
+  --           -- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
+  --           -- continuously displayed. If you only want a pop-up notification when the timer starts
+  --           -- and finishes, set this to false.
+  --           sticky = true,
+  --
+  --           -- Configure the display icons:
+  --           title_icon = "󱎫",
+  --           text_icon = "󰄉",
+  --           -- Replace the above with these if you don't have a patched font:
+  --           -- title_icon = "⏳",
+  --           -- text_icon = "⏱️",
+  --         },
+  --       },
+  --
+  --       -- The "System" notifier sends a system notification when the timer is finished.
+  --       -- Currently this is only available on MacOS.
+  --       -- Tracking: https://github.com/epwalsh/pomo.nvim/issues/3
+  --       -- { name = "System" },
+  --
+  --       -- You can also define custom notifiers by providing an "init" function instead of a name.
+  --       -- See "Defining custom notifiers" below for an example 👇
+  --       -- { init = function(timer) ... end }
+  --     },
+  --
+  --     -- Override the notifiers for specific timer names.
+  --     timers = {
+  --       -- For example, use only the "System" notifier when you create a timer called "Break",
+  --       -- e.g. ':TimerStart 2m Break'.
+  --       Break = {
+  --         { name = "System" },
+  --       },
+  --     },
+  --   }
+  --
+  -- },
 
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
