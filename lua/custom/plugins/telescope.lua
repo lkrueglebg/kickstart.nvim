13636445819b3289ef4c7d6483f226ccc83bcf20
lkrueglebg <lukas.kruegle@berkshiregrey.com>
2024-10-02 09:05:17 -0400

    telescope improvements

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,4 +1,19 @@
 -- Fuzzy Finder (files, lsp, etc)
+
+local function live_grep_with_src()
+  local cwd = vim.fn.getcwd()
+  local dirs_to_search = {cwd}
+  if vim.fn.isdirectory('src') then
+    for submodule in vim.fs.dir('src') do
+      if vim.fn.isdirectory(submodule) then
+        table.insert(dirs_to_search, cwd .. '/' .. submodule)
+      end
+    end
+  end
+  vim.print(dirs_to_search)
+  require('telescope.builtin').live_grep{search_dirs=dirs_to_search}
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
@@ -17,15 +32,21 @@ return {
       end,
     },
     {
-      "nvim-telescope/telescope-live-grep-args.nvim" ,
+      'nvim-telescope/telescope-live-grep-args.nvim' ,
       -- This will not install any breaking changes.
       -- For major updates, this must be adjusted manually.
-      version = "^1.0.0",
+      version = '^1.0.0',
     },
+    {
+      'cljoly/telescope-repo.nvim',
+    },
+    {
+      'airblade/vim-rooter'
+    }
 
   },
   config = function ()
-    require("telescope").load_extension("live_grep_args")
+    require('telescope').load_extension('live_grep_args')
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
     require('telescope').setup {
@@ -50,17 +71,17 @@ return {
       local current_dir
       local cwd = vim.fn.getcwd()
       -- If the buffer is not associated with a file, return nil
-      if current_file == "" then
+      if current_file == '' then
         current_dir = cwd
       else
         -- Extract the directory from the current file's path
-        current_dir = vim.fn.fnamemodify(current_file, ":h")
+        current_dir = vim.fn.fnamemodify(current_file, ':h')
       end
 
       -- Find the Git root directory from the current file's path
-      local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+      local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
       if vim.v.shell_error ~= 0 then
-        print("Not a git repository. Searching on current working directory")
+        print('Not a git repository. Searching on current working directory')
         return cwd
       end
       return git_root
@@ -90,13 +111,20 @@ return {
     vim.keymap.set('n', '<leader>sg', function()
 	require('telescope.builtin').git_files{recurse_submodules=true}
     end, { desc = '[S]earch [G]it Files' })
-    vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function ()
+      require('telescope.builtin').find_files{}
+    end , { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>st', require("telescope").extensions.live_grep_args.live_grep_args, { desc = '[S]earch by Text' })
+    vim.keymap.set('n', '<leader>st', live_grep_with_src, { desc = '[S]earch by Text' })
     vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
     vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>sq', require('telescope.builtin').quickfixhistory, { desc = '[S]earch [Q]uickfix history' })
+    vim.keymap.set('n', '<leader>sm', require('telescope.builtin').marks, { desc = '[S]earch [M]arks' })
+    vim.keymap.set('n', '<leader>sp', function ()
+      require('telescope').extensions.repo.list({search_dirs = {'~/devel', '~/code'}})
+    end, { desc = '[S]earch [P]rojects' })
 
   end
 }
